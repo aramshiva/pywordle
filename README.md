d0b3dfdca72b948ea8a17ebd687a1512caafd65e
Cbot <79479940+cboteleven@users.noreply.github.com>
2023-10-12 16:55:55 -0700

    Wordle in Python.
    Wordle In Python.

---
+++ README.md
@@ -0,0 +1,3 @@
+# pywordle: wordle in python
+Wordle In Python.
+Wordle Words are from tabatkins/wordle-list/words
--- wordle_engine.py
+++ wordle_engine.py
@@ -2,18 +2,14 @@
 import random
 import sys
 import time
-import bold
 
-class Styling:
-    clear = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+CLEAR = "\033[H\033[J"
 def Start():
-    finalresult = []
-    words = open("wordle.txt", "r") # Please install wordle.txt (14000 lines. A 2500 TechSmart Edition is also possible)
+    words = open("/Users/ashiva/Code/PythonCode/Data/wordle.txt", "r")
     words = words.read().splitlines()
-    class TimeChecker:  # Classes are new
-        current_day = time.strftime('%A') # They can hold data or functions and are called for by ClassName.var or ClassName.func
     word = random.choice(words)
-    bold.boldprint("WELCOME TO WORDLE".ljust(20))
+    finalresult = []
+    print("WELCOME TO WORDLE".ljust(20))
     print("BY ARAM".ljust(20))
     print()
     print("✅ means it is in the right spot".ljust(20))
@@ -21,19 +17,22 @@ def Start():
     print("❌ means it isnt in the word".ljust(20))
     print()
     input("Press ENTER key to start!\n")
-    print(Styling.clear)
-    #print("Todays wordle is " + word + ".") #  Can be used for testing purposes
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     Won = False
     for i in range(6):
             if not Won:
-                guess = input("what is your guess? type Q to quit\n")
-                if guess == "Q":
-                    sys.exit()
-                if len(guess) != 5:
-                    print("The word was " + word)
-                    raise Exception("The word must be 5 letters!")
+                Checker = True
+                while Checker:
+                    guess = input("what is your guess? type Q to quit\n")
+                    if guess == "Q":
+                        sys.exit()
+                    if len(guess) != 5:
+                        raise Exception("The word must be 5 letters!")
+                    if not guess.lower() in words:
+                        print("Not a word!")
+                    else:
+                        Checker = False
                 result = []
-                GuessCorrect = False
                 for i in range(5):
                     if guess[i] == word[i] or guess[i].upper() == word[i].upper():
                         result.append('✅')
@@ -45,23 +44,18 @@ def Start():
                 finalresult.append(" ".join(result))
             if guess == word:
                 break
-    print(Styling.clear)
+    print(CLEAR)
     print("WORDLE".center(20))
     print()
-    for i in finalresult:
-        print(i)
-    if guess == word:
-        print("Congrats! You guessed correct! Todays wordle was " + word.capitalize())
-        while TimeChecker.current_day == time.strftime('%A'):
-            pass
-    else:
-        bold.boldprint("You guessed uncorrectly! Todays wordle was " + word.capitalize())
-        print("The wordle will change tommorrow. You can keep this tab open or come back tommorow!")
-        while TimeChecker.current_day == time.strftime('%A'):
-            pass
-    while TimeChecker.current_day == time.strftime('%A'):
-        pass
+    for i in finalresult: print(i)
+    if guess == word: print("You guessed correct! Todays wordle was " + word.capitalize() + "\nThe wordle will change tommorrow. You can keep this tab open or come back tommorow!")
+    else: print("You guessed uncorrectly! Todays wordle was " + word.capitalize() + "\nThe wordle will change tommorrow. You can keep this tab open or come back tommorow!")
 def ChangeWordle():
     print("The wordle today has changed.")
-    word = random.choice(WordleStarter.words)
+    Start()
 Start()
+day = time.strftime('%A')
+while day == time.strftime('%A'):
+    day = time.strftime('%A')
+    pass
+ChangeWordle()
